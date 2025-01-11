36685acc7023db9a370a293c6543bdea5539e2f2
Boris <glimah481@gmail.com>
2025-01-11 11:24:37 +0300

    поменял даты

--- src/mocks/events.js
+++ src/mocks/events.js
@@ -12,8 +12,8 @@ const events = [
   {
     id: '2',
     basePrice: 90,
-    dateFrom: '2024-11-12T22:55:56.845Z',
-    dateTo: '2024-11-13T11:22:13.375Z',
+    dateFrom: '2025-11-12T22:55:56.845Z',
+    dateTo: '2025-11-13T11:22:13.375Z',
     destination: '1',
     isFavorite: false,
     offers: ['2', '4'],
@@ -22,8 +22,8 @@ const events = [
   {
     id: '3',
     basePrice: 900,
-    dateFrom: '2024-11-14T02:42:56',
-    dateTo: '2024-11-14T11:22:13',
+    dateFrom: '2025-11-14T02:42:56',
+    dateTo: '2025-11-14T11:22:13',
     destination: '2',
     isFavorite: true,
     offers: ['1', '2'],
@@ -32,8 +32,8 @@ const events = [
   {
     id: '4',
     basePrice: 300,
-    dateFrom: '2024-11-16T22:55:56',
-    dateTo: '2024-11-17T11:22:13',
+    dateFrom: '2025-11-16T22:55:56',
+    dateTo: '2025-11-17T11:22:13',
     destination: '4',
     isFavorite: false,
     offers: [],
